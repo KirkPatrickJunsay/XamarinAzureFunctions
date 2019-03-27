f3a0d31ce3b985573e634d8a5ba60cf146fae759
Kirk Patrick Junsay <kirk.patrick.junsay@osm.no>
2019-03-27 16:52:04 +0800

    Add project files.

---
+++ XamarinAzureFunctions/XamarinAzureFunctions/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinAzureFunctions
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonExecute_OnClicked(object sender, EventArgs e)
+        {
+            var azureFunction = "https://xamarinazurefunction.azurewebsites.net/api/xamarinhelloworld?code=" +
+                                $"Texc95fQqL3ZVcB9aV5Gqb95yTyUsQicnRIZhs8YKmmna/WRnlP92w==&name={entryName.Text}";
+            var httpClient = new HttpClient();
+
+            labelGreeting.Text = await httpClient.GetStringAsync(azureFunction);
+        }
+    }
+}
